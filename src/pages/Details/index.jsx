6c9262b39830b3748b90958eadea5d876256be7e
AlexandreRCS <ale.maresias@gmail.com>
2024-04-07 22:38:55 -0300

    Update

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,10 @@
-import "./style.css"
+import {Container} from './styles'
 
 export function Details(){
   return (
-    <>
+    <Container>
       <h1>Hello World</h1>
       <h2>Alexandre</h2>
-    </>
+    </Container>
   )
 }
