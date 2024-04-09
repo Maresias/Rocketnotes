b6f0727624d235d5d83f256bd1e2770958b2f018
AlexandreRCS <ale.maresias@gmail.com>
2024-04-08 22:38:12 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Details} from './pages/Details/'
+import { ThemeProvider } from 'styled-components'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Details />
+    <Details/>
   </React.StrictMode>,
 )
