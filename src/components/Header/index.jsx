e6815f6a3fda2fcb71f948ba531dbea80e4e626b
AlexandreRCS <ale.maresias@gmail.com>
2024-04-15 23:08:01 -0300

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,15 @@
-import { Container } from './style'
+import { Container, Profile } from './style'
 
 export function Header(){
     return (
         <Container>
             <Profile>
                 <img src="https://github.com/Maresias.png" alt="foto de uma pessoa" />
+
+                <div>
+                    <span>Bem-Vindo</span>
+                    <strong>Alexandre Correia</strong>
+                </div>
             </Profile>
         </Container>
     )
