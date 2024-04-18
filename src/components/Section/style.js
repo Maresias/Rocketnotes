d8d44b1c090544226e722871d57afcb7ea695588
AlexandreRCS <ale.maresias@gmail.com>
2024-04-17 22:23:11 -0300

    update

--- src/components/Section/style.js
+++ src/components/Section/style.js
@@ -4,12 +4,12 @@ export const Container = styled.section`
     margin: 5.6rem 0 2.8rem;
 
     >h2{
-        border: 1px solid ${({ theme }) => theme.CORLORS.BACKGROUND_700};
+        border: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
 
         padding-bottom: 1.6rem;
         margin-bottom: 2.4rem;
 
-        color: ${({ theme }) => theme.CORLORS.GRAY_100};
+        color: ${({ theme }) => theme.COLORS.GRAY_100};
         font-size: 2.0rem;
         font-weight: 400;
     }
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,6 +2,7 @@ import {Container} from './styles'
 
 
 import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
 import { Button } from '../../components/button'
 
 export function Details(){
@@ -9,6 +10,17 @@ export function Details(){
     <Container>
       
       < Header/>
+      
+      <Section title= "Links úteis" > 
+        <ul>
+          <li>Oi</li>
+          <li>cade</li>
+          <li>vice</li>
+        </ul>
+
+      </Section>
+
+
       <Button title="Voltar" />
 
     </Container>
