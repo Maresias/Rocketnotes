54de44f74adcd01b1da742013ff45d00f04b8f88
AlexandreRCS <ale.maresias@gmail.com>
2024-04-20 22:32:52 -0300

    Update index.jsx

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -16,13 +16,22 @@ export function Details(){
         <main>
             <Content>
              <ButtonText title="Excluir nota"/>
+
+             <h1>
+               Introdução ao React
+             </h1>
+
+             <p>
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta error consequuntur
+               aliquam dolorum minima quisquam perspiciatis exercitationem quis? Reprehenderit, 
+               harum! Voluptatem doloremque odit quidem itaque vel vitae placeat natus dolorum.
+             </p>
           
               <Section title= "Links úteis" > 
-              <Links>
-                <li><a href="#">https://www.rocketsest.com.br</a></li>
-                <li><a href="#">https://www.rocketsest.com.br</a></li>
-              </Links>
-
+                <Links>
+                  <li><a href="#">https://www.rocketsest.com.br</a></li>
+                  <li><a href="#">https://www.rocketsest.com.br</a></li>
+                </Links>
               </Section>
 
               <Section title="Marcadores">
