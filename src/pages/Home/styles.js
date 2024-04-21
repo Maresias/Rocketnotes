37f31523f1999db20b5d9afa632359782b6a3988
AlexandreRCS <ale.maresias@gmail.com>
2024-04-20 23:02:19 -0300

    update

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,12 +1,25 @@
 import styled from 'styled-components'
 
-export const Container  = styled.div``
+export const Container  = styled.div`
+    width: 100%;
+    height: 100vh;
+
+    display: grid;
+    grid-template-columns: 250px auto;
+    grid-template-rows: 105px 128px auto 64px;
+    grid-template-areas:
+     "brand header"
+     "menu  search"
+     "menu  content"
+     "newnote content"
+    ;
+`
 
 export const Brand  = styled.div``
 
 export const Menu  = styled.ul``
 
-export const Seach  = styled.div``
+export const Search  = styled.div``
 
 export const Content  = styled.div``
 
