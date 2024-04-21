c42fa764f76fb7e2d625020dfe47b38cc137ffdd
AlexandreRCS <ale.maresias@gmail.com>
2024-04-20 22:48:45 -0300

    Update index.jsx

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,25 @@
+import { Container, Brand, Menu, Seach, Content, NewNote } from './styles'
+
+import { Header } from '../../components/Header'
+
+export function Home(){
+    return (
+        <Container>
+            <Brand>
+               <h1>RocketNotes</h1>
+            </Brand>
+
+            <Menu>
+            </Menu>
+
+            <Seach>
+            </Seach>
+
+            <Content>
+            </Content>
+
+            <NewNote>
+            </NewNote>
+        </Container>
+    )
+}
