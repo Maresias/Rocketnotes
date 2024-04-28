10d380fe43accadee3b2e231b4699bf46618c52d
AlexandreRCS <ale.maresias@gmail.com>
2024-04-27 22:49:18 -0300

    Update style.js

--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -1,5 +1,31 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    
+    width: 100%;
+    display: flex;
+    align-items: center;
+
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+    color: ${({ theme }) => theme.COLORS.GRAY_300};
+
+    margin-bottom: .8rem;
+    border-radius: 1rem;
+
+    > input {
+        height: 5.6rem;
+        width: 100%auto;
+
+        padding: 1.2rem;
+        color: ${({ theme }) => theme.COLORS.WHITE};
+        background: transparent;
+        border: 0;
+
+        &placeholder {
+            color: ${({ theme }) => theme.COLORS.GRAY_300};
+        }
+
+        > SVG {
+            margin-left: 1.6rem;
+        }
+    }
 `
