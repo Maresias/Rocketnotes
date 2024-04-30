5192b2b1a9394a3f54d0cf746c44acb37175d2b4
AlexandreRCS <ale.maresias@gmail.com>
2024-04-29 23:08:31 -0300

    update

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,12 @@
+import { FiMAil, FiLock} from 'react-icons'
+
+import { Input } from "../../components/Input"
+import { Button } from '../../components/button'
+
+export function SignUp (){
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
