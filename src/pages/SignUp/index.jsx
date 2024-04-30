cce7714e3b28be1dcdfa8f9dc5bc5788d93bd71c
AlexandreRCS <ale.maresias@gmail.com>
2024-04-29 23:14:03 -0300

    Update index.jsx

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -8,8 +8,30 @@ export function SignUp (){
     return (
         <Container>
             <Form>
-                
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+                <h2>Faça seu login</h2>
+
+                <Input
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                />
+
+                <Input
+                    placeholder="Senha"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Button>
+                    Entrar
+                </Button>
+
+                <a href="#"> Criar Conta</a>
             </Form>
+            <Background/>
         </Container>
     )
 }
