f0d9169d5479078e17eb75f9fc9c2aae6d59e8c7
AlexandreRCS <ale.maresias@gmail.com>
2024-04-30 22:32:08 -0300

    Update index.jsx

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,8 @@
-import { FiArrowLeft } from "react-icons/fi";
-import { Container } from "./styles";
+import { FiArrowLeft, FiUser, FiMail, FiLock } from "react-icons/fi";
+import { Container, Form } from "./styles";
+
+
+import { Input  } from '../../components/Input'
 
 
 export function Profile(){
@@ -10,6 +13,28 @@ export function Profile(){
                     <FiArrowLeft/>
                 </a>
             </header>
+
+            <Form>
+                <Input
+                    placeholder="Nome"
+                    type="text"
+                    icon={FiUser}
+                />
+
+                <Input
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                />
+
+                <Input
+                    placeholder="Senha Atual"
+                    type="password"
+                    icon={FiLock}
+                />
+
+            </Form>
+
         </Container>
     )
 }
