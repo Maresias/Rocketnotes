cdaceffe4c692b59d1cde40fc2a1cc9220eeedbf
AlexandreRCS <ale.maresias@gmail.com>
2024-04-30 22:24:10 -0300

    Update styles.js

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -2,4 +2,21 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
     width: 100%;
+
+    > header {
+        width: 100%;
+        height: 14.4rem;
+
+        background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+        display: flex;
+        align-items: center;
+
+        padding: 0 12.4rem;
+
+        svg {
+            color: ${({ theme}) => theme.COLORS.GRAY_100};
+            font-size: 2.4rem;
+        }
+    }
 `
