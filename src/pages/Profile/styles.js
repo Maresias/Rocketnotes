5748d407bcfe49673f3ef49293c155e8acadcd33
AlexandreRCS <ale.maresias@gmail.com>
2024-05-01 21:31:34 -0300

    Update styles.js

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -24,8 +24,50 @@ export const Container = styled.div`
 export const Form = styled.form`
     max-width: 340px;
     margin: 3rem auto 0;
+
+    > div:nth-child(4){
+        margin-top:2.4rem;
+    }
 `
 
 export const Avatar = styled.div`
-    
+    position: relative;
+    margin: -11.4rem auto 3.2rem;
+
+    width: 186px;
+    height: 186px;
+
+    > img {
+        width: 100%;
+        border-radius: 50%;
+    }
+
+    > label {
+        width: 4.8rem;
+        height: 4.8rem;
+
+        background-color: ${({ theme }) => theme.COLORS.ORANGE};
+        border-radius: 50%;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        position: absolute;
+        bottom: 0.7rem;
+        right: 0.7rem;
+
+        cursor: pointer;
+
+        input {
+            display: none;
+        }
+
+        svg {
+            width: 2rem;
+            height: 2rem;
+
+            color: ${({ theme}) => theme.COLORS.BACKGROUND_800};
+        }
+    }
 `
