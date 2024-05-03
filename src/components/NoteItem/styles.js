52a7ef39e7659508c8cc33d610f1ce1552d7b427
AlexandreRCS <ale.maresias@gmail.com>
2024-05-03 00:48:02 -0300

    Update styles.js

--- src/components/NoteItem/styles.js
+++ src/components/NoteItem/styles.js
@@ -5,5 +5,20 @@ export const Container = styled.div`
     align-items: center;
 
     background-color: ${({ theme, isNew}) => isNew ?  "transparent" : theme.COLORS.BACKGROUND_900};
-    color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+    color: ${({ theme }) => theme.COLORS.GRAY_300};
+
+    border: ${({ theme, isNew }) => isNew ? `1px dashed ${theme.COLORS.GRAY_300}` : "none"};
+    margin-bottom: .8rem;
+    border-radius: 1rem;
+    padding-right: 1.6rem;
+
+    > button {
+        border:none;
+        background: none;
+    }
+
+    > input {
+        height: 5.6rem;
+        width:100%;
+    }
 `
