8817bccc48e4f7353bc0e1d49717eb20d624581c
AlexandreRCS <ale.maresias@gmail.com>
2024-05-03 00:52:40 -0300

    Update styles.js

--- src/components/NoteItem/styles.js
+++ src/components/NoteItem/styles.js
@@ -20,5 +20,15 @@ export const Container = styled.div`
     > input {
         height: 5.6rem;
         width:100%;
+
+        padding: 1.2rem;
+        color:${({ theme }) => theme.COLORS.WHITE};
+        background:transparent;
+
+        border: none;
+
+        &::placeholder{
+            color: ${({ theme }) => theme.COLORS.GRAY_300};
+        }
     }
 `
