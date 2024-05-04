e6c49340f60620732c1b2f70842194875388f6c5
AlexandreRCS <ale.maresias@gmail.com>
2024-05-03 21:43:52 -0300

    Update auth.routes.jsx

--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,3 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 
-im
+import { New } from '../pages/SignIn'
+import { Home } from '../pages/SignUp'
+
+export function AppRoutes(){
+    return (
+        <Routes>
+            <Route path='/' element={ <Home />} />
+            <Route path='/new' element={ <New/> } />
+        </Routes>
+    )
+}
