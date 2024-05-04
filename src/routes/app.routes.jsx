ee44c93200a49cb3f7d8592b3658e0a6627ee190
AlexandreRCS <ale.maresias@gmail.com>
2024-05-03 21:26:23 -0300

    Update app.routes.jsx

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -4,3 +4,14 @@ import { New } from '../pages/New'
 import { Home } from '../pages/Home'
 import { Profile } from '../pages/Profile'
 import { Details } from '../pages/Details'
+
+export function AppRoutes() {
+    return (
+        <Routes>
+            <Router path="/" element={ <Home />} />
+            <Router path="/new" element={ <New />} />
+            <Router path="/profile" elemnt={ <Profile />} />
+            <Router path="/details" elemnt={ <Details />} />
+        </Routes>
+    )
+}
