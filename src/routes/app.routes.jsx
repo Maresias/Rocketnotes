a893d6e8e95e028dab30c3b613fc0735e643b9b1
AlexandreRCS <ale.maresias@gmail.com>
2024-05-03 21:20:31 -0300

    Update app.routes.jsx

---
+++ src/routes/app.routes.jsx
@@ -0,0 +1,6 @@
+import { Routes, Router} from 'react-router-dom'
+
+import { New } from '../pages/New'
+import { Home } from '../pages/Home'
+import { Profile } from '../pages/Profile'
+import { Details } from '../pages/Details'
