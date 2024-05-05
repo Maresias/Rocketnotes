804ae892046c71ecaccb8f7b15a74adcc42c33ce
AlexandreRCS <ale.maresias@gmail.com>
2024-05-05 20:48:31 -0300

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import { Container, Profile, Logout } from './style'
 export function Header(){
     return (
         <Container>
-            <Profile>
+            <Profile to="/profile">
                 <img src="https://github.com/Maresias.png" alt="foto de uma pessoa" />
 
                 <div>
