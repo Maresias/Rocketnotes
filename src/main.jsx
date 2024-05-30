d06eae2612ba0c3b793a521a2ee6cfe63b9728dc
AlexandreRCS <ale.maresias@gmail.com>
2024-05-29 22:24:38 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 
-import theme from './styles/theme'
+import { MyContext } from './myContext'
 
 import { Routes } from './routes'
+
+import theme from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
