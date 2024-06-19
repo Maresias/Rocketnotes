583e07113cd7452731ce01db5a5538341fc30396
AlexandreRCS <ale.maresias@gmail.com>
2024-06-19 08:27:04 -0300

    Update

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
 import { Textarea } from '../../components/Textarea'
@@ -9,6 +11,18 @@ import { Container, Form} from './styles'
 import { Link } from 'react-router-dom'
 
 export function New() {
+    const [links, setLinks ] = useState([])
+    const [newLink, setNewLink] = useState("")
+
+    function handleAddLink(){
+        setLinks( prevState => [ ...prevState, newLink ])
+        setNewLink("")
+    }
+
+    function handleRemoveLink(deleted){
+        setLinks( prevState => prevState.filter( link => link !== deleted ))
+    }
+
     return (
         <Container>
             <Header/>
@@ -25,9 +39,22 @@ export function New() {
                     <Textarea placeholder="Observações"/>
 
                     <Section title="Links úteis">
-
-                        <NoteItem value="https://rocketseat.com.br"/>
-                        <NoteItem  placeholder="Novo link" isNew/>
+                        {
+                            links.map((link, index)=> (
+                                <NoteItem
+                                key={String(index)}
+                                value={link}
+                                onClick={()=> handleRemoveLink(link)}
+                            />
+                            ))
+                        }
+                        <NoteItem 
+                         isNew
+                         placeholder="Novo Link"
+                         value={newLink}
+                         onChange={ e => setNewLink( e.target.value)}
+                         onClick={handleAddLink}
+                        />
 
                     </Section>
 
