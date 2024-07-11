0ed9401aa308c75304ffce8e81f28bc13b7ed767
AlexandreRCS <ale.maresias@gmail.com>
2024-07-10 21:51:43 -0300

    Update index.jsx

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -6,6 +6,7 @@ import { Textarea } from '../../components/Textarea'
 import { NoteItem } from '../../components/NoteItem'
 import { Section } from '../../components/Section'
 import { Button } from '../../components/button'
+import { ButtonText } from '../../components/ButtonText'
 
 import { api } from '../../services/api'
 import { useNavigate } from 'react-router-dom'
@@ -24,6 +25,10 @@ export function New() {
     const [ newTag, setNewTag ] = useState("")
 
     const navigate = useNavigate()
+
+    function handleBack(){
+        navigate(-1)
+    }
 
     function handleAddLink(){
         setLinks( prevState => [ ...prevState, newLink ])
@@ -63,7 +68,7 @@ export function New() {
             links
         })
         alert("Nota cadastrada com sucesso !!!")
-        navigate("/")
+        navigate(-1)
     }
 
     return (
@@ -74,7 +79,7 @@ export function New() {
                 <Form>
                     <header>
                         <h1>Criar nota</h1>
-                        <Link to="/">Voltar</Link>
+                        <ButtonText title={"Voltar"} onClick={handleBack}/>
                     </header>
 
                     <Input 
